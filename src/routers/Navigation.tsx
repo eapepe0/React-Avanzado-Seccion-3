e7fa0afab115c2233e94ab177b4471d3ce2a5671
eapepe0 <eapepe0@gmail.com>
2023-03-13 16:16:19 -0300

    Leccion 39 , finalizada. LazyLoad implementado y Suspense

--- src/routers/Navigation.tsx
+++ src/routers/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 import logo from "../logo.svg";
@@ -5,34 +6,36 @@ import { routes } from "./routes";
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React Logo" />
+    <Suspense fallback={<span>Cargando ...</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
 
-          <ul>
-            {/* mapeamos routes , extraemos to y name , usamos de key el to , y el name es lo que mostrariamos en el menu lateral */}
-            {routes.map(({ to, name }) => (
-              <li key={to}>
-                <NavLink
-                  to={to}
-                  className={({ isActive }) => (isActive ? "nav-active" : "")}
-                >
-                  {name}
-                </NavLink>
-              </li>
+            <ul>
+              {/* mapeamos routes , extraemos to y name , usamos de key el to , y el name es lo que mostrariamos en el menu lateral */}
+              {routes.map(({ to, name }) => (
+                <li key={to}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {/* mapeamos el route extraemos el path y el componente , mostramos cada route donde el element es el Componente pasado */}
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
             ))}
-          </ul>
-        </nav>
-        <Routes>
-          {/* mapeamos el route extraemos el path y el componente , mostramos cada route donde el element es el Componente pasado */}
-          {routes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component />} />
-          ))}
-          {/* como ruta con comodin , navegamos al to de la primera ruta  */}
-          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            {/* como ruta con comodin , navegamos al to de la primera ruta  */}
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,30 +1,39 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { lazy, LazyExoticComponent } from "react";
+
+type JSXComponent = () => JSX.Element;
 
 //* como queremos que se vean las rutas
 interface Route {
   to: string;
   path: string;
   name: string;
-  Component: () => JSX.Element; //* un componente es una funcion que regresa un elemento JSX
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent; //* un componente es una funcion que regresa un elemento JSX
 }
+
+//* LAZY LOAD
+
+const Lazy1 = lazy(() => import("../01-lazyload/pages/LazyPage1"));
+const Lazy2 = lazy(() => import("../01-lazyload/pages/LazyPage2"));
+const Lazy3 = lazy(() => import("../01-lazyload/pages/LazyPage3"));
+
 export const routes: Route[] = [
   //* ponemos la interface Route
   {
     to: "/lazy1", //* esto tiene que coincidir con el to del <NavLink/>
     path: "lazy1", //* es igual al path de <Route>  tanto path como to tienen que coincidir
-    Component: LazyPage1, //* el componente que renderizara , tiene que tener la C mayuscula , el componente siempre se debe crear con mayuscula
+    Component: Lazy1, //* El lazyLoad que se cargara
     name: "Lazy-1", //* nombre que visualmente querramos ver
   },
   {
     to: "/lazy2", //* esto tiene que coincidir con el to del <NavLink/>
     path: "lazy2", //* es igual al path de <Route>  tanto path como to tienen que coincidir
-    Component: LazyPage2, //* el componente que renderizara , tiene que tener la C mayuscula , el componente siempre se debe crear con mayuscula
+    Component: Lazy2, //* El lazyLoad que se cargara
     name: "Lazy-2",
   },
   {
     to: "/lazy3", //* esto tiene que coincidir con el to del <NavLink/>
     path: "lazy3", //* es igual al path de <Route>  tanto path como to tienen que coincidir
-    Component: LazyPage3, //* el componente que renderizara , tiene que tener la C mayuscula , el componente siempre se debe crear con mayuscula
+    Component: Lazy3, //* El lazyLoad que se cargara
     name: "Lazy-3",
   },
 ];
