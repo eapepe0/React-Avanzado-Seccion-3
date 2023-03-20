3037712662b378de9592d09a6c5871a944021786
eapepe0 <eapepe0@gmail.com>
2023-03-20 03:46:15 -0300

    Leccion 60 , Finalizada,Recibir Props al componente hijo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,20 +1,33 @@
-import styles from "../styles/styles.module.css";
-import noImage from "../assets/no-image.jpg";
-import { useState } from "react";
 import { useProduct } from "../hooks/useProduct";
 
-export const ProductCard = () => {
+import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
+
+//* definimos como se verian las props , en este caso recibimos un product, el cual se ve como la interface Product
+interface Props {
+  product: Product;
+}
+
+//* definimos como se veria el producto
+interface Product {
+  id: string; //* el id sera una string
+  title: string; //* el titulo sera una string
+  img?: string; //*la imagen puede o no existir (?) , sera una string
+}
+
+export const ProductCard = ({ product }: Props) => {
+    
   const { counter, increaseBy } = useProduct();
+
   return (
     <div className={styles.productCard}>
-      {/* <img className={styles.productImg} src={noImage} alt="Coffee Mug" /> */}
-      {/* producto sin imagen */}
       <img
         className={styles.productImg}
-        src="./coffee-mug.png"
+        src={product.img ? product.img : noImage} //* le decimos al src que si existe el img , lo usamos de lo contrario usamos el noImage
         alt="Coffee Mug"
       />
-      <span className={styles.productDescription}>Coffee Mug</span>
+      <span className={styles.productDescription}>{product.title}</span>{" "}
+      {/*  mostramos el titulo*/}
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
           -
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,41 @@
 import { ProductCard } from "../components/ProductCard";
 
+//* creamos el objeto product
+
+const product = {
+  id: "1",
+  title: "Coffee Mug - Card",
+  img: "./coffee-mug.png",
+};
+
+const productNoImage = {
+  id: "1",
+  title: "Coffee Mug - Card",
+};
+
 export const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {/* typescript nos dice que tenemos que mandarle algo llamado product */}
+        <ProductCard product={product} />
+        <ProductCard product={product} />
+        <ProductCard product={productNoImage} />
+        <ProductCard product={product} />
+        <ProductCard product={productNoImage} />
+        <ProductCard product={product} />
+        <ProductCard product={product} />
+        <ProductCard product={productNoImage} />
+      </div>
     </div>
   );
 };
