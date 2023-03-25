b9e6627108e62f1a0e6c427b92ec99f11357777d
eapepe0 <eapepe0@gmail.com>
2023-03-25 04:20:46 -0300

    Leccion 81 , Finalizada , Comienzo Control Props

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -8,9 +8,10 @@ const product = {
   img: "./coffee-mug.png",
 };
 
-const productNoImage = { 
-  id: "1",
-  title: "Coffee Mug - Card",
+const product2 = { 
+  id: "2",
+  title: "Coffee Mug - Meme",
+  img : "./coffee-mug2.png"
 };
 
 export const ShoppingPage = () => {
@@ -33,19 +34,11 @@ export const ShoppingPage = () => {
 
 
 
-        <ProductCard product={product} className='bg-dark'>
+        <ProductCard product={product2} className='bg-dark'>
             <ProductImage className="custom-image"/>
             <ProductTitle className="text-white text-center text-bold" />
             <ProductButtons className="custom-button"/>
         </ProductCard>
-        
-
-        <ProductCard product={product} style={{backgroundColor : "gray"}}>
-            <ProductImage style={{filter : "blur(1.6px)" }}/>
-            <ProductTitle style={{textShadow : "5px 10px "}}/>
-            <ProductButtons style={{display: "flex" , justifyContent: "flex-end" }}/>
-        </ProductCard>
-        
       </div>
     </div>
   );
@@ -53,11 +46,11 @@ export const ShoppingPage = () => {
 
 
 /**
- * //* linea 3 :creamos el objeto product
- * //* linea 9 creamos un producto el cual no tenga una imagen , solamente para probar
+ * //* linea 3  :creamos el objeto product
+ * //* linea 11 : creamos otro objeto product
+ *  
+ * //* linea 22 : agregamos unos estilos en linea , style={{}}
  * 
- * //* linea 19 : agregamos unos estilos en linea , style={{}}
  * 
- * //* linea 26 : TypeScript nos dice que tenemos que mandarle algo llamado product 
  * 
  */
