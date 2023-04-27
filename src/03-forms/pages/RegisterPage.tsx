c9fbb6152b0374bf6a66fbdce1e02d267e75934c
eapepe0 <eapepe0@gmail.com>
2023-04-27 20:01:10 -0300

    Leccion 127, Formulario tradicional con useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import "../styles/styles.css";
 
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: "Cristian",
+    email: "c@gmail.com",
+    password1: "123456",
+    password2: "123456",
+  });
+
+  const { name, email, password1, password2 } = registerData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
   return (
     <div>
       <h1>Register Page</h1>
       <form action="">
-        <input type="text" name="Name" id="name" placeholder="Name" />
-        <input type="email" name="Email" id="email" placeholder="Email" />
+        <input
+          type="text"
+          name="Name"
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={onChange}
+        />
+        <input
+          type="email"
+          name="Email"
+          id="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
         <input
           type="password"
           name="password1"
           id="password1"
           placeholder="Password"
+          value={password1}
+          onChange={onChange}
         />
         <input
           type="password"
           name="password2"
           id="password2"
           placeholder="Repeat Password"
+          value={password2}
+          onChange={onChange}
         />
         <button type="submit">Create</button>
       </form>
