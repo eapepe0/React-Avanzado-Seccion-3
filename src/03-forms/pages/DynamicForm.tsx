dfc3c4a298f3a581c678c0222b6002b6bdf93185
eapepe0 <eapepe0@gmail.com>
2023-05-02 11:36:44 -0300

    Validaciones dinamicas

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,14 +1,31 @@
 import formJson from '../data/custom-form.json'
 
 import { Formik , Form } from 'formik'
+import * as Yup from "yup";
 import { MySelect, MyTextInput } from '../components'
 
 
-const initialValues : {[key : string ] : any}= {};
+const initialValues : { [key : string ] : any } = { }; //* le decimos a TS que es un objeto vacio que recibe una key la cual es una string y un valor que pude ser cualquier cosa
+const requiredFields : { [key : string ] : any } = { };
 
-for (const input of formJson) {
-    initialValues[ input.name ] = input.value;
+//* recorremos cada valor del JSON
+for ( const input of formJson ) {
+    initialValues[ input.name ] = input.value; //* creamos el initialValues
+
+    if( !input.validations) continue; //* si no existe ninguna validacion en el JSON seguimos con el loop sin hacer nada
+
+    let schema = Yup.string(); //* creamos un Yup string no un objeto
+
+    for (const rule of input.validations ) { //* recorremos el input.validations
+        if (rule.type === 'required') { 
+             schema = schema.required('Este campo es requerido'); //* es igual al mismo schema por que puede haber varias reglas en el mismo input
+        }
+    }
+    requiredFields[ input.name ] = schema; //* creamos el objeto para este campo
+    console.log(typeof(requiredFields))    
 }
+
+const validationSchema = Yup.object( { ...requiredFields } ) //* creamos el objeto de Yup haciendo un spread del objeto que creamos antes
 
 export const DynamicForm = () => {
     
@@ -21,6 +38,7 @@ export const DynamicForm = () => {
         onSubmit={ (values) =>
             console.log(values)
         }
+        validationSchema = { validationSchema }
 
         >
             {(formik)=>(
@@ -37,11 +55,12 @@ export const DynamicForm = () => {
                         return (
                              <MySelect key={name} label={label} name={name}>
                                 <option value="" >Elija una opcion</option>
-                               {/*  {
+                               {/*  { //* si las opciones las enviamos como un array ["opc 1","opc2"]
                                     options?.map((opt) => (
                                         <option key={opt} value={opt}>{opt}</option>
                                     ))
-                                } */}
+                                } */
+                                }
                                 {
                                     options?.map(({id , label}) => (
                                         <option key={id} value={label}>{label}</option>
